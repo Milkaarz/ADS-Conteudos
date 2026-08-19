2c6472594a01f5f24bff1246d13c6a5a33edfc03
Giovana Oliveira <lpsgiovana@gmail.com>
2026-08-19 19:03:08 -0300

    Update Vogal ou Consoante.cpp

--- Semestre 3 /Estrutura de Dados /Exercicios/Vogal ou Consoante.cpp
+++ Semestre 3 /Estrutura de Dados /Exercicios/Vogal ou Consoante.cpp
@@ -5,14 +5,14 @@ using namespace std;
 int main() {
     char x;
 
-    cout << "Insira uma letra: ";
-    cin >> x;
+    std::cout << "Insira uma letra: " << std::endl;
+    std::cin >> x;
 
     if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u' ||
         x == 'A' || x == 'E' || x == 'I' || x == 'O' || x == 'U') {
         cout << "Essa letra e uma vogal" << endl;
     } else {
-        cout << "Essa letra e uma consoante" << endl;
+        std::cout << "Essa letra e uma consoante" << endl;
     }
 
     return 0;
